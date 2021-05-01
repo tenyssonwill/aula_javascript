3dd672d660e37bc67fa3d7c41e28f87991d6ae74
Tenysson <tenyssonwill@gmail.com>
2021-05-01 15:30:44 -0300

    new file:   index.html 	new file:   js/main.js

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/*var nome = "Tenysson Will";
+var idade = 40;
+var idade2 = 10;
+
+console.log("Meu nome é " + nome.toUpperCase() + " e tenho " +  (idade + idade2) + " anos.");*/
+
+var lista = ["maça","pêra","laranja"]
+//lista.pop();
+//lista.push("uva")
+//lista.reverse();
+//lista.length;
+//lista.toString();
+//lista.join(";")
+
+var fruta = "mamao";
+
+function pega(){
+    fruta = "pera";
+}
+
+pega();
+
+console.log(fruta);
+//alert("oi");
